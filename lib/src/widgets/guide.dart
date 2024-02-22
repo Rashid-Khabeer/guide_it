4c3c28f2c0a3aba7885dd9c91ad5214d46f595a2
Rashid-khabeer <rkhabeer84@gmail.com>
2024-02-22 13:22:42 +0500

    Added Back and next buttons

--- lib/src/widgets/guide.dart
+++ lib/src/widgets/guide.dart
@@ -18,7 +18,9 @@ class Guide extends StatefulWidget {
     this.onComplete,
     this.autoShow = true,
     this.showAlways = false,
+    this.disableAutoNext = false,
   });
+
   final GuideDisplayOptions displayOptions;
   final void Function(String guid)? onComplete;
   final bool Function(String guid)? canShow;
@@ -36,19 +38,23 @@ class Guide extends StatefulWidget {
   final GuideController? controller;
   final List<GuideItem> items;
   final Widget child;
+  final bool disableAutoNext;
 
   @override
   State<Guide> createState() => _GuideState();
 }
 
 class _GuideState extends State<Guide> {
-  StreamSubscription? _showSub;
+  StreamSubscription<ShowGuideParams>? _showSub;
+  StreamSubscription<GuideActions>? _actionsSub;
   late GuideController _controller;
+
   @override
   void initState() {
     super.initState();
     _controller = widget.controller ?? GuideController();
     _showSub = _controller.show.listen(show);
+    _actionsSub = _controller.actions.listen(actions);
     if (!widget.autoShow) return;
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       if (_shouldShow()) {
@@ -61,12 +67,17 @@ class _GuideState extends State<Guide> {
   void dispose() {
     super.dispose();
     _showSub?.cancel();
+    _actionsSub?.cancel();
   }
 
   bool _shouldShow() {
     if (widget.showAlways) return true;
     return widget.canShow?.call(widget.guid) ?? true;
   }
+
+  int currentActive = 0;
+  List<(GuideItem, OverlayEntry)> overlays = [];
+  OverlayState? overlayState;
 
   @override
   Widget build(BuildContext context) => widget.child;
@@ -76,9 +87,7 @@ class _GuideState extends State<Guide> {
       action.onComplete?.call();
       return;
     }
-    OverlayState overlayState = Overlay.of(context);
-    List<(GuideItem, OverlayEntry)> overlays = [];
-    int currentActive = 0;
+    overlayState = Overlay.of(context);
     for (final item in widget.items) {
       overlays.add(
         (
@@ -90,6 +99,7 @@ class _GuideState extends State<Guide> {
                 defaultDisplayOptions: widget.displayOptions,
                 item: item,
                 onTap: () {
+                  if (widget.disableAutoNext) return;
                   overlays[currentActive].$2.remove();
                   for (currentActive = currentActive + 1;
                       currentActive < overlays.length;
@@ -98,7 +108,7 @@ class _GuideState extends State<Guide> {
                     if (currentActive != overlays.length &&
                         current.$1.targetWidgetKey != null &&
                         current.$1.targetWidgetKey!.currentWidget != null) {
-                      overlayState.insert(current.$2);
+                      overlayState?.insert(current.$2);
                       break;
                     }
                   }
@@ -124,8 +134,41 @@ class _GuideState extends State<Guide> {
             element.$1.targetWidgetKey!.currentWidget != null,
       );
       if (currentActive >= 0) {
-        overlayState.insert(overlays[currentActive].$2);
+        overlayState?.insert(overlays[currentActive].$2);
       }
     });
+  }
+
+  Future<void> actions(GuideActions action) async {
+    if (overlays.isEmpty) return;
+    switch (action) {
+      case GuideActions.back:
+        if (currentActive == 0) return;
+        overlays[currentActive].$2.remove();
+        for (var i = currentActive - 1; i >= 0; i--) {
+          var previous = overlays[i];
+          if (previous.$1.targetWidgetKey != null &&
+              previous.$1.targetWidgetKey!.currentWidget != null) {
+            overlayState?.insert(previous.$2);
+            currentActive = i;
+            break;
+          }
+        }
+        break;
+      case GuideActions.next:
+        overlays[currentActive].$2.remove();
+        for (currentActive = currentActive + 1;
+            currentActive < overlays.length;
+            currentActive++) {
+          var current = overlays[currentActive];
+          if (currentActive != overlays.length &&
+              current.$1.targetWidgetKey != null &&
+              current.$1.targetWidgetKey!.currentWidget != null) {
+            overlayState?.insert(current.$2);
+            break;
+          }
+        }
+        break;
+    }
   }
 }
